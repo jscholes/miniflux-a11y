68b9c1e79a310e31b94731cb801aef61ff1147a0
James Scholes <james@jamesscholes.com>
2025-05-12 14:28:26 -0600

    Unread view: add the relative timestamp, category name, and feed title to the accessible name of each unread item

--- miniflux-a11y.js
+++ miniflux-a11y.js
@@ -1,7 +1,15 @@
 const UNREAD_PATH = '/unread';
 
+let globalIDCounter = 0;
+
 function init() {
 	if (window.location.pathname === UNREAD_PATH) {
+		// Create some hidden text nodes to hold labelling parts.
+		const colonNode = makeHiddenTextNode('a11y-colon', ':');
+		const semicolonNode = makeHiddenTextNode('a11y-semicolon', ';');
+		const fromNode = makeHiddenTextNode('a11y-from', 'from');
+		const inNode = makeHiddenTextNode('a11y-in', 'in');
+
 		const itemsContainer = document.querySelector('div.items');
 
 		// Make this a focusable list so screen readers switch modes.
@@ -9,9 +17,23 @@ function init() {
 		itemsContainer.tabIndex = '-1';
 		itemsContainer.setAttribute('aria-labelledby', 'page-header-title');
 
-		// Give all items a list item role.
+		// Give all items a list item role, and make their accessible names more descriptive.
 		itemsContainer.querySelectorAll('article').forEach(item => {
 			item.role = 'listitem';
+
+			let labelledBy = item.getAttribute('aria-labelledby');
+			labelledBy = `${labelledBy} ${colonNode.id}`;
+
+			const relativeTimeID = assignAutoID(item.querySelector('time'));
+			labelledBy = `${labelledBy} ${relativeTimeID}`;
+
+			const categoryID = assignAutoID(item.querySelector('.category > a'));
+			labelledBy = `${labelledBy} ${inNode.id} ${categoryID}`;
+
+			const feedTitleID = assignAutoID(item.querySelector('.item-meta-info-title > a'));
+			labelledBy = `${labelledBy} ${semicolonNode.id} ${feedTitleID}`;
+
+			item.setAttribute('aria-labelledby', labelledBy);
 		});
 
 		// Focus the first unread item.
@@ -19,6 +41,25 @@ function init() {
 		firstItem.classList.add('current-item');
 		firstItem.focus();
 	}
+}
+
+function makeHiddenTextNode(id, text) {
+	const node = document.createElement('span');
+
+	node.setAttribute('id', id);
+	node.hidden = true;
+	node.textContent = text;
+
+	document.body.appendChild(node);
+
+	return node;
+}
+
+function assignAutoID(element) {
+	const newID = `a11y-auto-${globalIDCounter}`;
+	element.setAttribute('id', newID);
+	globalIDCounter++;
+	return newID;
 }
 
 window.addEventListener('load', init);
